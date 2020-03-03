060d572844f26cb1f8d8695afa47cc82cc0dec6c
Andreas M. Munk <andreas.m.m64@gmail.com>
2020-03-03 13:09:09 -0800

    update website with cool buttons and puzzles

---
+++ assets/js/button-swap.js
@@ -0,0 +1,6 @@
+$(".spoiler-button").on("click", function() {
+    var el = $(this);
+    el.text() == el.data("text-swap")
+        ? el.text(el.data("text-original"))
+        : el.text(el.data("text-swap"));
+});
