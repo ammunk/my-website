fbafcb0f4ebda8242b57c73fea39dbb690b98da8
Andreas Munk <andreas@ammunk.com>
2020-10-25 02:02:14 -0700

    merge and update website

--- assets/js/snippet-to-copy.js
+++ assets/js/snippet-to-copy.js
@@ -1,5 +1,5 @@
 var snippets=document.querySelectorAll('.snippet-to-copy');
-[].forEach.call(snippets,function(snippet){snippet.firstChild.insertAdjacentHTML('beforebegin','<button class="btn" data-clipboard-snippet><img class="clippy" width="13" src="/assets/octicon_svg/clippy.svg" alt="Copy to clipboard"></button>');});
+[].forEach.call(snippets,function(snippet){snippet.firstChild.insertAdjacentHTML('beforebegin','<button class="btn copy-btn btn-outline-secondary btn-rounded shadow-5-strong" data-clipboard-snippet><i class="clippy fas fa-copy pl-1" alt="Copy to clipboard"></i></button>');});
 
 var clipboardSnippets=new ClipboardJS('[data-clipboard-snippet]',{target:function(trigger){return trigger.nextElementSibling;}});
 
@@ -8,12 +8,12 @@ clipboardSnippets.on('success',function(e){e.clearSelection();
 clipboardSnippets.on('error',function(e){showTooltip(e.trigger,fallbackMessage(e.action));});
 
 // This relies on the primer.css for tooltips to work!
-var btns=document.querySelectorAll('.btn');
+var btns=document.querySelectorAll('.copy-btn');
 for(var i=0;i<btns.length;i++){btns[i].addEventListener('mouseleave',clearTooltip);
                                btns[i].addEventListener('blur',clearTooltip);}
-function clearTooltip(e){e.currentTarget.setAttribute('class','btn');
+function clearTooltip(e){e.currentTarget.setAttribute('class','btn copy-btn btn-outline-secondary btn-rounded shadow-5-strong');
                          e.currentTarget.removeAttribute('aria-label');}
-function showTooltip(elem,msg){elem.setAttribute('class','btn tooltipped tooltipped-s tooltipped-no-delay');
+function showTooltip(elem,msg){elem.setAttribute('class','btn copy-btn btn-outline-secondary btn-rounded shadow-5-strong tooltipped tooltipped-s tooltipped-no-delay');
                                elem.setAttribute('aria-label',msg);}
 function fallbackMessage(action){var actionMsg='';
                                  var actionKey=(action==='cut'?'X':'C');
